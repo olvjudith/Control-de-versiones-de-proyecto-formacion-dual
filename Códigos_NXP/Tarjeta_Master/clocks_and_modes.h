2b72dd85e0a1479f24080fdca3ff8afd5e6ce444
Giovanni Espinosa <giovanniespinosabernal@gmail.com>
2019-12-11 01:03:17 -0600

    Se añaden los códigos para el proyecto de software de las tarjetas master y slave con formato, también el reporte de la práctica de comunicación serial SPI

--- Códigos_NXP/Tarjeta_Master/clocks_and_modes.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLOCKS_AND_MODES_H_
-#define CLOCKS_AND_MODES_H_
-
-extern void SOSC_init_8MHz (void);
-extern void SPLL_init_160MHz (void);
-extern void NormalRUNmode_80MHz (void);
-
-#endif /* CLOCKS_AND_MODES_H_ */
-
